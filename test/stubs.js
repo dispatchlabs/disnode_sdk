3f7d66270b4e13ea55780fcd176dd6fb982f4234
David Hutchings <chefslide@gmail.com>
2018-09-12 23:21:45 -0700

    Updating for TN-2.2.0

--- test/stubs.js
+++ test/stubs.js
@@ -15,6 +15,7 @@ module.exports = (DisNodeSDK) => {
         address: undefined,
         privateKey: undefined,
         publicKey: undefined,
+        transaction: undefined,
         balance: undefined,
         created: undefined,
         updated: undefined
@@ -24,6 +25,7 @@ module.exports = (DisNodeSDK) => {
         address: 'fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30d',
         privateKey: '472ba91402425b58a2eebf932812f20c6d7f6297bba1f83d9a58116ae6512d9e',
         publicKey: '04775936b80a436491a386fbdbea04603b12689e3e2600085ecf956dc4dd1bed45240eb7fcabfebc98f24d28c5862d3e8a9d9a4b26265f35b727b98db24d9f0566',
+        transaction: undefined,
         balance: 5,
         created: undefined,
         updated: undefined
@@ -43,20 +45,21 @@ module.exports = (DisNodeSDK) => {
         abi: '[]',
         method: 'foo',
         params: [],
-        signature: 'b82f8165a22fb9a7cc46e862ac83042e1295119f98eb940428074fe49396109f1eaa7087f2623a0abbaafa9a35d3ac19c6086fd96f4a1eec7ef683373425d6f900',
+        gossip: [],
+        signature: '2c46bc93214d0d867b007ca5418b724eb3cd44ed5363b909401f20e7a0da42c145ef2906221dd1e284e654b3f11b69e350793e44d41253ea1ad22e3db1d4a81601',
         hertz: 0,
         fromName: undefined,
         toName: undefined,
         address: '03e08bb6f3f25c3374487c4f5c026bd1e0fa4f5a39ef89e52f5ec1cb93a5c59744'
       },
-      T1_str: '{"hash":"1d6aca3a91c9ad81f36d0e52e47992257ba4c5309916ca370a2a37e9abad2e1f","type":0,"from":"fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30d","to":"fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30b","value":5,"code":"a","abi":"[]","method":"foo","params":[],"time":1532595045435,"signature":"b82f8165a22fb9a7cc46e862ac83042e1295119f98eb940428074fe49396109f1eaa7087f2623a0abbaafa9a35d3ac19c6086fd96f4a1eec7ef683373425d6f900","hertz":0,"address":"03e08bb6f3f25c3374487c4f5c026bd1e0fa4f5a39ef89e52f5ec1cb93a5c59744"}',
+      T1_str: '{"hash":"1d6aca3a91c9ad81f36d0e52e47992257ba4c5309916ca370a2a37e9abad2e1f","type":0,"from":"fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30d","to":"fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30b","value":5,"code":"a","abi":"[]","method":"foo","params":[],"time":1532595045435,"signature":"2c46bc93214d0d867b007ca5418b724eb3cd44ed5363b909401f20e7a0da42c145ef2906221dd1e284e654b3f11b69e350793e44d41253ea1ad22e3db1d4a81601","hertz":0,"address":"03e08bb6f3f25c3374487c4f5c026bd1e0fa4f5a39ef89e52f5ec1cb93a5c59744","gossip":[]}',
       type0: {
         from: 'fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30d',
         to: 'fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30b',
         value: 5,
         time: 1532595045435
       },
-      type0Hash: '4dbd495353f5022e7f1fb9577477bab6052dd25ff80136a0c85a5961385e7e94',
+      type0Hash: 'd2412aabc150bc16e88b02a2a4763a3fbd7795cb95ff767bf4b01c4f2f15c029',
       type1: {
         type: 1,
         from: 'fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30d',
@@ -73,7 +76,13 @@ module.exports = (DisNodeSDK) => {
               stateMutability: 'nonpayable',
               type: 'function'
             }
-          ]
+          ],
+        receipt: {
+            transactionHash: 'e39762885605dc05844a0f62f6b715bbd7a365f2fd31dd45af15f0e6941e3236',
+            status: 'Ok',
+            created: '2018-07-27T18:14:40.809768288Z',
+            contractAddress: 'fa61c18114f8ff8aafbeb5d32e1b108e3f6cf30c'
+          }
       },
       type1Hash: 'e39762885605dc05844a0f62f6b715bbd7a365f2fd31dd45af15f0e6941e3236',
       type2: {
@@ -84,13 +93,13 @@ module.exports = (DisNodeSDK) => {
         method: 'g',
         abi: []
       },
-      type2Hash: '7679d83c92cb7629d3e645f63907e14f7d0da60bf37b0e04558bf0b8e02347fe'
+      type2Hash: 'd6fc08b2e466c6b8210e53efa8d4fd94ac5ebaf92d78391987c8d23d0aca480b'
     },
     Contract: {
       C1: {
-        source: 'contract x { function g() { } }',
+        source: 'pragma solidity ^0.4.24; contract x { function g() { } }',
         name: 'x',
-        bytecode: '6080604052348015600f57600080fd5b5060858061001e6000396000f300608060405260043610603e5763ffffffff7c0100000000000000000000000000000000000000000000000000000000600035041663e2179b8e81146043575b600080fd5b348015604e57600080fd5b5060556057565b005b5600a165627a7a72305820aee1d9928562f46bc9cc3a8951bb2655ba01f6682a0b09d556774a7992a4c5470029',
+        bytecode: '6080604052348015600f57600080fd5b5060858061001e6000396000f300608060405260043610603e5763ffffffff7c0100000000000000000000000000000000000000000000000000000000600035041663e2179b8e81146043575b600080fd5b348015604e57600080fd5b5060556057565b005b5600a165627a7a723058206b231aaad800171db67b40801bc5efc9022f2f3246cac8acea51d0113a3af9e00029',
         abi: [
             {
               constant: false,
@@ -145,17 +154,12 @@ module.exports = (DisNodeSDK) => {
         }
       })
       break;
-    case '/v1/receipts/e39762885605dc05844a0f62f6b715bbd7a365f2fd31dd45af15f0e6941e3236':
+    case '/v1/transactions/e39762885605dc05844a0f62f6b715bbd7a365f2fd31dd45af15f0e6941e3236':
       callback(null, {
         status: 'Ok',
         humanReadableStatus: 'Ok',
         created: '2018-07-27T18:14:40.809768288Z',
-        data: {
-          transactionHash: 'e39762885605dc05844a0f62f6b715bbd7a365f2fd31dd45af15f0e6941e3236',
-          status: 'Ok',
-          created: '2018-07-27T18:14:40.809768288Z',
-          contractAddress: stubData.Contract.C1.address
-        }
+        data: stubData.Transaction.type1
       });
       break;
     default:
